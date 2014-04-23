d54317b5002c58b16f3b463d4b848dcdb2cdb971
Diego <diego-neumann@hotmail.com>
2014-04-23 16:34:08 -0300

    BUGFIX: Ajustado retorno contendo a URL da imagem no crop.

--- TESTE_API_LOCAL/application/controllers/backend/upload_mig_file_json.php
+++ TESTE_API_LOCAL/application/controllers/backend/upload_mig_file_json.php
@@ -4,7 +4,7 @@ class Upload_mig_file_json extends CI_Controller_Auth_JSON {	public function in
 			$data=array("errorCode"=>1 ,"errorDesc"=>"Erro subindo imagem.São permitidos apenas imagens JPG e de 1770px X 1110px ");
 			echo json_encode($data);
 			return;
-		}		$imgUp = $this->upload->data();		$data=array("errorCode"=>0 ,"errorDesc"=>"success","data"=>_BASEURL_."/public/img/temp_upload/{$imgUp["file_name"]}","dataName"=>$imgUp["file_name"]);		echo json_encode($data);
+		}		$imgUp = $this->upload->data();		$data=array("errorCode"=>0 ,"errorDesc"=>"success","data"=>_HTTP_BASEPATH_."/public/img/temp_upload/{$imgUp["file_name"]}","dataName"=>$imgUp["file_name"]);		echo json_encode($data);
 		
 		/////Remove todos os arquivos da pasta temp_upload
 		$diretorio = dir($upload_path);
